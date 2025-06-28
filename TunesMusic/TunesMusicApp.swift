69c8cd5997f94dced8e8abc31353cd5ec361393d
emporehezertama <emporeht@gmail.com>
2025-06-28 08:54:30 +0700

    Initial Commit

---
+++ TunesMusic/TunesMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  TunesMusicApp.swift
+//  TunesMusic
+//
+//  Created by Fajar Subeki on 28/06/25.
+//
+
+import SwiftUI
+
+@main
+struct TunesMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TunesMusicTests/TunesMusicTests.swift
@@ -0,0 +1,17 @@
+//
+//  TunesMusicTests.swift
+//  TunesMusicTests
+//
+//  Created by Fajar Subeki on 28/06/25.
+//
+
+import Testing
+@testable import TunesMusic
+
+struct TunesMusicTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
